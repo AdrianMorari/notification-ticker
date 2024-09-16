391a1173a33c3e821367b3f4bf16183259eb88d9
Adrian Morari <adrian.morari@tbc.md>
2024-09-16 14:33:51 +0300

    Small update login window

--- main.js
+++ main.js
@@ -68,23 +68,17 @@ app.setAsDefaultProtocolClient('notification-ticker');
 
 ipcMain.on('open-google-login', (event) => {
     const loginWindow = new BrowserWindow({
-        width: 400,
-        height: 600,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
     });
-    loginWindow.loadURL('https://accounts.google.com');
+    loginWindow.loadURL('https://staging-bo.tmgbo.com/');
 
     loginWindow.webContents.on('did-finish-load', () => {
         setTimeout(() => {
             event.sender.send('google-login-success', { token: 'fake-google-token' });
             loginWindow.close();
-        }, 3000);
+        }, 10000);
     });
-});
-
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
 });
